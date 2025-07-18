af9e1248c62ddd2e7354cc93283d387be8e5cf81
ahmedcherif11 <ahmed.cherif2@insat.ucar.tn>
2025-07-18 01:44:44 -0400

    normalize

--- data-processing/scripts/llm-pretrain/shuffle.py
+++ data-processing/scripts/llm-pretrain/shuffle.py
@@ -2,7 +2,7 @@ import random
 
 
 
-input_path ="/scratch/cherif/dataset/data_fixed.jsonl"
+input_path ="/scratch/cherif/dataset/data_fixed_normalized.jsonl"
 output_path = "/scratch/cherif/dataset/data_fixed_shuffled.jsonl"
 
 with open(input_path, "r") as infile:
