b97f937c97077457bed888ab1ef931b52e6f3c6a
ahmedcherif11 <ahmed.cherif2@insat.ucar.tn>
2025-07-31 11:31:25 -0400

    wandb&&

--- data-processing/scripts/sh-scripts/sync.sh
+++ data-processing/scripts/sh-scripts/sync.sh
@@ -16,7 +16,7 @@ if [ -z "$MODEL" ]; then
 fi
 
 if [ -z "$MODELPATH" ]; then
-  MODELPATH="$SCRATCH/models/run/llama-3.1-8B-ftdwindows4K-adapter"
+  MODELPATH="$SCRATCH/models/run/llama-3.1-8B-ftdwindows4K-adapter2"
   echo "Model path not specified, using default: $MODELPATH"
 fi
 
