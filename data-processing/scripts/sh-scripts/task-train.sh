011c821050ebdc793228ff8e79949a94bffe22ee
ahmedcherif11 <ahmed.cherif2@insat.ucar.tn>
2025-08-08 23:49:26 -0400

    task

--- data-processing/scripts/sh-scripts/task-train.sh
+++ data-processing/scripts/sh-scripts/task-train.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=80000                  # More RAM for big batches/context
 #SBATCH --cpus-per-task=16           # Use more CPUs for dataloader
 #SBATCH --output=$SCRATCH/models/slurm-logs/%N-%j.out
-#SBATCH --time=20:00:00
+#SBATCH --time=32:00:00
 #SBATCH --account=def-dmouheb  
 #SBATCH --mail-user=ahmed.cherif.1@ulaval.ca
 #SBATCH --mail-type=ALL    
